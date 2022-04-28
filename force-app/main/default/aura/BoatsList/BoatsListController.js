87d749247d992aa2b91ed29b9f43a711f78d4a3e
FabienD <phwright26@gmail.com>
2022-04-28 09:20:09 +0200

    display Boats + Maintenances in aura component

--- force-app/main/default/aura/BoatsList/BoatsListController.js
+++ force-app/main/default/aura/BoatsList/BoatsListController.js
@@ -1,5 +1,6 @@
 ({
-    doInit : function(component, event, helper) {
+    // méthode qui retourne la liste des bateaux
+    /*doInit : function(component, event, helper) {
 
         // récup l'action de BoatsList.cmp
         const action = component.get('c.getAllBoats');
@@ -18,7 +19,79 @@
                 // faire passer la valeur retournée au front
                 component.set("v.boats", boats);
 
-                console.log(JSON.parse(boats, null, 2));
+                console.log(JSON.stringify(boats));
+
+            } else {
+                // gestion d'erreur
+                console.log(state);
+            }
+
+        });
+
+        // transmet l'action au serveur apex
+        $A.enqueueAction(action);
+
+        console.log("init called");
+
+    }*/
+
+    // méthode qui retourne la liste des bateaux avec leur type
+    /*doInit : function(component, event, helper) {
+
+        // récup l'action de BoatsList.cmp
+        const action = component.get('c.getAllBoatsWithTypes');
+
+        // set le callback (toujours la ref this en 1er parametre, et une fonction de callback en 2e apram)
+        action.setCallback(this, function(response) {
+
+            const state = response.getState();
+
+            console.log(state);
+
+            if(state === "SUCCESS") {
+                // récup la valeur retournée
+                const boats = response.getReturnValue();
+
+                // faire passer la valeur retournée au front
+                component.set("v.boats", boats);
+
+                console.log(JSON.stringify(boats));
+
+            } else {
+                // gestion d'erreur
+                console.log(state);
+            }
+
+        });
+
+        // transmet l'action au serveur apex
+        $A.enqueueAction(action);
+
+        console.log("init called");
+
+    }*/
+
+    // méthode qui retourne la liste des bateaux avec leur maintenance
+    doInit : function(component, event, helper) {
+
+        // récup l'action de BoatsList.cmp
+        const action = component.get('c.getAllBoatsWithMaintenances');
+
+        // set le callback (toujours la ref this en 1er parametre, et une fonction de callback en 2e apram)
+        action.setCallback(this, function(response) {
+
+            const state = response.getState();
+
+            console.log(state);
+
+            if(state === "SUCCESS") {
+                // récup la valeur retournée
+                const boats = response.getReturnValue();
+
+                // faire passer la valeur retournée au front
+                component.set("v.boats", boats);
+
+                console.log(JSON.stringify(boats));
 
             } else {
                 // gestion d'erreur
@@ -33,4 +106,5 @@
         console.log("init called");
 
     }
+
 })
